b722994ea9f943e41bcba66fc7ced7cb8642e74a
Aruna-DataAces <120357453+Aruna-DataAces@users.noreply.github.com>
2025-07-21 13:05:40 +0530

    Update streamlit_app.py
    Pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import pandas as pd
 import requests
 
 # from snowflake.snowpark.context import get_active_session
@@ -27,6 +28,9 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 ingredients_list = st.multiselect(
     "Choose upto 5 ingredients",
     my_dataframe,
